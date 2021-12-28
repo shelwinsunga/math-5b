4a7d8a99b68b74c79f00637c17e1c2a2814d629b
Shelwin Sunga <shelwinsunga@gmail.com>
2021-12-28 07:05:40 -0800

    Added more problems and solutions to power rule

--- components/chapter6math.js
+++ components/chapter6math.js
@@ -12,11 +12,29 @@ export const ProblemA = () => <InlineMath math={'\\ f(x) = 15x^{100} - 3x^{12} +
 export const SolutionA = () => <InlineMath math={'\\ f\ \\ \'\\left( x \\right)   = 1500{x^{99}} - 36{x^{11}} + 5'} />;
 
 export const ProblemB = () => <InlineMath math={'g\\left( t \\right) = 2{t^{6}} + 7{t^{ - 6}}'} />;
-export const SolutionB = () => <InlineMath math={'\\ f\ \\ \'\\left( x \\right)   = 1500{x^{99}} - 36{x^{11}} + 5'} />;
+export const SolutionB = () => <InlineMath math={'\\ g\'\\left( t \\right) = 12{t^5} - 42{t^{ - 7}}'} />;
 
 export const ProblemC = () => <InlineMath math={'\\displaystyle y = 8{z^3} - \\frac{1}{{3{z^5}}} + z - 23'} />;
+export const SolutionC = () => <InlineMath math={'\\displaystyle y\' = 24{z^2} + \\frac{5}{3}{z^{ - 6}} + 1'} />;
+
+
 export const ProblemD = () => <InlineMath math={'\\displaystyle T\\left( x \\right) = \\sqrt x  + 9\\sqrt[3]{{{x^7}}} - \\frac{2}{{\\sqrt[5]{{{x^2}}}}} '} />;
+export const SolutionD = () => <InlineMath math={'\\displaystyle T\'\\left( x \\right) = \\frac{1}{2}{x^{ - \\,\\,\\frac{1}{2}}} + \\frac{{63}}{3}{x^{\\frac{4}{3}}} + \\frac{4}{5}{x^{ - \\,\\,\\frac{7}{5}}}' } />;
+
 export const ProblemE = () => <InlineMath math={'\\ h\\left( x \\right) = {x^\\pi } - {x^{\\sqrt 2 }}'} />;
+export const SolutionE = () => <InlineMath math={'h\'\\left( x \\right) = \\pi {x^{\\pi  - 1}} - \\sqrt 2 {x^{\\sqrt 2  - 1}}'} />;
+
+
+export const ProblemF = () => <InlineMath math={'\\ h\\left( x \\right) = {x^\\pi } - {x^{\\sqrt 2 }}'} />;
+export const SolutionF = () => <InlineMath math={'h\'\\left( x \\right) = \\pi {x^{\\pi  - 1}} - \\sqrt 2 {x^{\\sqrt 2  - 1}}'} />;
+
+
+export const ProblemG = () => <InlineMath math={'\\ h\\left( x \\right) = {x^\\pi } - {x^{\\sqrt 2 }}'} />;
+export const SolutionG = () => <InlineMath math={'h\'\\left( x \\right) = \\pi {x^{\\pi  - 1}} - \\sqrt 2 {x^{\\sqrt 2  - 1}}'} />;
+
+
+export const ProblemH = () => <InlineMath math={'\\ h\\left( x \\right) = {x^\\pi } - {x^{\\sqrt 2 }}'} />;
+export const SolutionH = () => <InlineMath math={'h\'\\left( x \\right) = \\pi {x^{\\pi  - 1}} - \\sqrt 2 {x^{\\sqrt 2  - 1}}'} />;
 
 
 
